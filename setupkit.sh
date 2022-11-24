3f504d62c40de451d5167cc0adee6ba15c91ee0c
Raisan <yuri@rainyan.net>
2022-11-25 00:05:01 +0900

    Update setupkit.sh

--- setupkit.sh
+++ setupkit.sh
@@ -8,6 +8,9 @@ sudo apt install -y vim python3-pip
 sudo python3 -m pip --upgrade pip
 sudo python3 -m pip install glances
 
+# Firmware Update
+sudo rpi-update
+
 # Reboot
 echo "\x1b[31mWarning: Reboot after 5s !\x1b[0m"
 sleep 10s
