7c22f04f64efa227b51be2ea2a9b7bc9a0b8e068
Raisan <yuri@rainyan.net>
2022-11-25 00:00:08 +0900

    Create setupkit.sh

---
+++ setupkit.sh
@@ -0,0 +1,14 @@
+# Update
+sudo apt update
+sudo apt upgrade -y
+sudo apt dist-upgrade
+
+# Install
+sudo apt install -y vim python3-pip
+sudo python3 -m pip --upgrade pip
+sudo python3 -m pip install glances
+
+# Reboot
+echo "\x1b[31mWarning: Reboot after 5s !\x1b[0m"
+sleep 10s
+sudo reboot
